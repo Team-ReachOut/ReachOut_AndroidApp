aaace09cb08ede9a86e5945b0b69723b8531f71e
Ishaan Dhamija <ishaandhamija@Ishaans-MacBook-Air.local>
2017-08-10 13:08:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/ishaandhamija/reachout/Activities/HomeScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.ishaandhamija.reachout.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ishaandhamija.reachout.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnSignup = (Button) findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/ishaandhamija/reachout/Activities/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.ishaandhamija.reachout.Activities;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.ishaandhamija.reachout.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    TextView title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        title = (TextView) findViewById(R.id.title);
+
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/font2.ttf");
+        title.setTypeface(typeFace);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, HomeScreenActivity.class));
+            }
+        }, 5000);
+
+    }
+}
