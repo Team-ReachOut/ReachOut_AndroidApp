988f23737c731af53c0d5deed56ed7332e45b40e
Ishaan Dhamija <ishaandhamija@Ishaans-MacBook-Air.local>
2017-08-11 00:59:13 +0530

    database connected to android app

--- app/src/main/java/com/example/ishaandhamija/reachout/Activities/LoginActivity.java
+++ app/src/main/java/com/example/ishaandhamija/reachout/Activities/LoginActivity.java
@@ -1,15 +1,87 @@
 package com.example.ishaandhamija.reachout.Activities;
 
+import android.content.Intent;
+import android.support.constraint.solver.SolverVariable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ishaandhamija.reachout.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button btn_login;
+
+    public static final String TAG = "volley";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("email", email.getText().toString());
+                    json.put("password", password.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.43.202:5000/api/show",
+                        json,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    if (response == null){
+                                        Toast.makeText(LoginActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Name : " + response.get("name") + "\n" + "BloodGroup : " + response.get("bloodgroup"), Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(LoginActivity.this, abc.class);
+                                        i.putExtra("name", response.get("name").toString());
+                                        i.putExtra("bloodgroup", response.get("bloodgroup").toString());
+                                        startActivity(i);
+                                    }
+                                } catch (Exception e) {
+                                    Log.d(TAG, "onResponse: " + e.toString());
+                                    Toast.makeText(LoginActivity.this, "Dikkat", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                                Toast.makeText(LoginActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/ishaandhamija/reachout/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/ishaandhamija/reachout/Activities/SignUpActivity.java
@@ -2,14 +2,86 @@ package com.example.ishaandhamija.reachout.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ishaandhamija.reachout.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText name, age, sex, bloodgroup, address, contactno, email, password;
+    Button sign_up_button;
+
+    public static final String TAG = "volley";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        name = (EditText) findViewById(R.id.name);
+        age = (EditText) findViewById(R.id.age);
+        sex = (EditText) findViewById(R.id.sex);
+        bloodgroup = (EditText) findViewById(R.id.bloodgroup);
+        address = (EditText) findViewById(R.id.address);
+        contactno = (EditText) findViewById(R.id.contactno);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        sign_up_button = (Button) findViewById(R.id.sign_up_button);
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("name", name.getText().toString());
+                    json.put("bloodgroup", bloodgroup.getText().toString());
+                    json.put("email", email.getText().toString());
+                    json.put("phonenumber", contactno.getText().toString());
+                    json.put("address", address.getText().toString());
+                    json.put("age", age.getText().toString());
+                    json.put("sex", sex.getText().toString());
+                    json.put("password", password.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.43.202:5000/api/addone",
+                        json,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    Toast.makeText(SignUpActivity.this, "Signed Up", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "onResponse: ");
+                                    Toast.makeText(SignUpActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                                } catch (Exception e) {
+                                    Toast.makeText(SignUpActivity.this, "Dikkat", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        });
+                RequestQueue requestQueue = Volley.newRequestQueue(SignUpActivity.this);
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+
     }
 }
