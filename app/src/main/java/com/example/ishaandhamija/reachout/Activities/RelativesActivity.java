962f0cc123fdb4ee28120843f3eb47ad21456ea0
sarthakmathur <sarthakmathur97@gmail.com>
2017-09-26 23:34:05 +0530

    add relative api hit done and minor change

--- app/src/main/java/com/example/ishaandhamija/reachout/Activities/RelativesActivity.java
+++ app/src/main/java/com/example/ishaandhamija/reachout/Activities/RelativesActivity.java
@@ -1,23 +1,35 @@
 package com.example.ishaandhamija.reachout.Activities;
 
 import android.app.Dialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ishaandhamija.reachout.Models.Relative;
 import com.example.ishaandhamija.reachout.R;
 import com.example.ishaandhamija.reachout.Utils.RelativesAdapter;
 import com.jaredrummler.materialspinner.MaterialSpinner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -30,6 +42,12 @@ public class RelativesActivity extends AppCompatActivity implements View.OnClick
     Button btnAdd;
     ArrayList<Relative> relatives;
     RelativesAdapter relativesAdapter;
+    public static final String TAG = "RelativesActivity";
+    SharedPreferences sharedPreferences;
+    Dialog memberDialog;
+    public static final String MyPREFERENCES = "MyPrefs";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +62,7 @@ public class RelativesActivity extends AppCompatActivity implements View.OnClick
         mList.setLayoutManager(new LinearLayoutManager(this));
         mList.addItemDecoration(new DividerItemDecoration(RelativesActivity.this,LinearLayoutManager.VERTICAL));
         mList.setAdapter(relativesAdapter);
-
+        sharedPreferences = getSharedPreferences(MyPREFERENCES,MODE_PRIVATE);
         fab.setOnClickListener(this);
 
     }
@@ -52,7 +70,7 @@ public class RelativesActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View view) {
         if(view == fab){
-            final Dialog memberDialog = new Dialog(RelativesActivity.this);
+            memberDialog = new Dialog(RelativesActivity.this);
             memberDialog.getWindow().setBackgroundDrawable(null);
             memberDialog.setContentView(R.layout.dialog_add_member);
             WindowManager.LayoutParams lp = memberDialog.getWindow().getAttributes();
@@ -62,7 +80,7 @@ public class RelativesActivity extends AppCompatActivity implements View.OnClick
             lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
             window.setAttributes(lp);
             lp.gravity = Gravity.CENTER;
-            memberDialog.setCancelable(false);
+            memberDialog.setCancelable(true);
             memberDialog.show();
             name = (EditText) memberDialog.findViewById(R.id.name);
             age = (EditText) memberDialog.findViewById(R.id.age);
@@ -85,10 +103,56 @@ public class RelativesActivity extends AppCompatActivity implements View.OnClick
                     Relative relative = new Relative(mname,mage,mbGroup);
                     relatives.add(relative);
                     relativesAdapter.notifyDataSetChanged();
-//                    Toast.makeText(RelativesActivity.this, "Added!!", Toast.LENGTH_SHORT).show();
+                    addRelative(mname,mage,mbGroup);
                     memberDialog.dismiss();
                 }
             });
         }
     }
+    private void addRelative(String mname,String mage,String mbGroup) {
+
+        String useremail = sharedPreferences.getString("savedEmail"," ");
+        String userpassword = sharedPreferences.getString("savedPassword"," ");
+        JSONObject json = new JSONObject();
+        try {
+            json.put("relativeName", mname);
+            json.put("relativeAge", mage);
+            json.put("relativeBloodgroup", mbGroup);
+            json.put("email", useremail);
+            json.put("password", userpassword);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://192.168.43.202:5199/api/updateRelative",
+//        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://harshgoyal.xyz:5199/api/addone",
+//        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "https://reach-out-server.herokuapp.com/api/addone",
+                json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d(TAG, "onResponse: " + response.toString());
+                        } catch (Exception e) {
+                            Toast.makeText(RelativesActivity.this, "No response", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RelativesActivity.this, "Failed to Add!!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "onErrorResponse: " + error);
+                        error.printStackTrace();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(RelativesActivity.this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
 }
